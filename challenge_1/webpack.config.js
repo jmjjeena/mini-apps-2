3b16f2140100ef1a8668873b0277f2cb63cacef0
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-08 15:16:57 -0700

    Add webpack

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, "/client");
+const DIST_DIR = path.join(__dirname, "/public/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  }
+};
