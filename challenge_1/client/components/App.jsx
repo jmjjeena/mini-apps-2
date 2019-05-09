b2170348583c673f857b5903de92c59eff6ac887
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-08 18:49:12 -0700

    Add react-pagination

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import { PageHeader } from 'antd';
 import { Input } from 'antd';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
+
   render() {
     const Search = Input.Search;
     return (
@@ -15,12 +17,26 @@ class App extends Component {
         <PageHeader
           title="Historical Events Finder"
         />
-        <div>
+        <div className="search">
           <Search
-            className="search"
             placeholder="input search text"
             onSearch={value => console.log(value)}
             style={{ width : 200}}
+          />
+        </div>
+        <div className='react-paginate'>
+          <ReactPaginate
+            previousLabel={'<'}
+            nextLabel={'>'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            subContainerClassName={'pages pagination'}
+            activeClassName={'active'}
           />
         </div>
       </div>
