c78f6dcde5cb2484975a8d1f4c17df6fed5eb648
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-08 17:42:27 -0700

    And antd

--- challenge_1/client/index.jsx
+++ challenge_1/client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
-
+import "antd/dist/antd.css";
 
 ReactDom.render(<App />, document.getElementById('app'));
 
