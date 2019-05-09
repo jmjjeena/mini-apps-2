958500856201211fe27b210a32eb218305f1db59
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-08 17:42:14 -0700

    Add style-loader and css-loader

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -5,8 +5,8 @@ const DIST_DIR = path.join(__dirname, "/public/dist");
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
-    path: DIST_DIR
+    filename: 'bundle.js',
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -16,11 +16,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
         },
       },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
     ],
-  }
+  },
 };
