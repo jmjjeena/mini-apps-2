07b86b7f3ba43ef2028ad4ef44de9357cb25ec56
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-10 14:33:41 -0700

    Add event handlers for search

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -1,47 +1,105 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ReactPaginate from 'react-paginate';
+import SearchBar from './SearchBar.jsx';
+import ResultsList from './ResultsList.jsx';
+import Pagination from './Pagination.jsx';
 import { PageHeader } from 'antd';
-import { Input } from 'antd';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      searchWord:'',
+      results: [],
+      pageCount: 1,
+      currentPage: 1,
+      // loading: true
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handlePageClick =  this.handlePageClick.bind(this);
+  }
+
+  handleSearch(word) {
+    const { currentPage } = this.state;
+    axios.get(`/events?q=${word}&_page=${currentPage}`)
+    .then((response) => {
+      this.setState({
+        results: response.data,
+        // loading: false,
+        searchWord: word,
+        pageCount: Math.ceil(res.headers["x-total-count"] / 10)
+      });
+    })
+    .catch((error) => {
+      console.log('Oops! something went wrong', error);
+    });
+  }
+
+  handlePageClick(page) {
+    const { searchWord, currentPage } = this.state;
+    let selected = page.selected + 1;
+    this.setState({
+      currentPage: selected
+    }, () => {
+      this.handleSearch(searchWord, currentPage);
+    })
   }
 
   render() {
-    const Search = Input.Search;
+    const { results, pageCount } = this.state;
+    console.log('results: ', results);
     return (
       <div>
-        <PageHeader
-          title="Historical Events Finder"
-        />
-        <div className="search">
-          <Search
-            placeholder="input search text"
-            onSearch={value => console.log(value)}
-            style={{ width : 200}}
+        <div>
+          <PageHeader
+            title="Historical Events Finder"
           />
         </div>
-        <div className='react-paginate'>
-          <ReactPaginate
-            previousLabel={'<'}
-            nextLabel={'>'}
-            breakLabel={'...'}
-            breakClassName={'break-me'}
-            pageCount={this.state.pageCount}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={this.handlePageClick}
-            containerClassName={'pagination'}
-            subContainerClassName={'pages pagination'}
-            activeClassName={'active'}
+        <div>
+          <SearchBar handleSearch={this.handleSearch} />
+        </div>
+        <div>
+          <ResultsList results={results}/>
+        </div>
+        <div>
+          <Pagination
+            pageCount={pageCount}
+            handlePageClick={this.handlePageClick}
           />
         </div>
       </div>
-     );
-  }
-}
+
+    );
+  };
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <PageHeader
+  //         title="Historical Events Finder"
+  //       />
+  //       <div className="search">
+  //         <SearchBar />
+  //       </div>
+  //       <div className='react-paginate'>
+  //         <ReactPaginate
+  //           previousLabel={'<'}
+  //           nextLabel={'>'}
+  //           breakLabel={'...'}
+  //           breakClassName={'break-me'}
+  //           pageCount={this.state.pageCount}
+  //           marginPagesDisplayed={2}
+  //           pageRangeDisplayed={5}
+  //           onPageChange={this.handlePageClick}
+  //           containerClassName={'pagination'}
+  //           subContainerClassName={'pages pagination'}
+  //           activeClassName={'active'}
+  //         />
+  //       </div>
+  //     </div>
+  //    );
+  // }
+};
 
 export default App;
