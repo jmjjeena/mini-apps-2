fa62eee161b2939aa9169932181fc75fc02701a2
Jeena Chemparathy <jeena@Jeenas-MBP.hsd1.ca.comcast.net>
2019-05-12 17:40:33 -0700

    Create minesweeper reducer test

---
+++ challenge_4/src/reducers/minesweeperReducer.test.js
@@ -0,0 +1,47 @@
+import { ADD_BOARD } from '../actions/boardActions.js';
+import minesweeperReducer, { emptyBoard } from './minesweeperReducer.js';
+import store from './store.js'
+
+it('defines a board', () => {
+  expect(minesweeperReducer().board).toBeDefined();
+});
+
+describe('emptyBoard', () => {
+  it('builds an object with 9 keys', () => {
+    expect(Object.keys(emptyBoard(3)).length).toEqual(9);
+  });
+});
+
+describe('ADD_BOARD', () => {
+  const action = {
+    type: ADD_BOARD,
+    size: 3,
+    mineLocations: ['1,1', '2,2']
+  };
+
+  let newState;
+
+  beforeEach(() => {
+    newState = minesweeperReducer(store, action);
+  });
+
+  // it ('sets the correct hasMine values for the board', () => {
+  //   expect(newState.board).toBeDefined();
+  // });
+
+  it('sets the correct true hasMine values for the board', () => {
+    expect(newState.board['1,1'].hasMine).toEqual(true);
+    expect(newState.board['2,2'].hasMine).toEqual(true);
+  });
+
+  it('sets the correct true hasMine values for the board', () => {
+    expect(newState.board['0,0'].hasMine).toEqual(false);
+    expect(newState.board['0,1'].hasMine).toEqual(false);
+    expect(newState.board['0,2'].hasMine).toEqual(false);
+    expect(newState.board['1,0'].hasMine).toEqual(false);
+    expect(newState.board['1,2'].hasMine).toEqual(false);
+    expect(newState.board['2,0'].hasMine).toEqual(false);
+    expect(newState.board['2,1'].hasMine).toEqual(false);
+  });
+
+});
